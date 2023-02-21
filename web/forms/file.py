07e151726ab6b7099c8948ad7b3206f68ebdf391
养乐多要吸管 <a102467@outlook.com>
2023-02-21 21:08:02 +0800

    New folder function implementation

--- web/forms/file.py
+++ web/forms/file.py
@@ -1,5 +1,7 @@
 
 from django import forms
+from django.core.exceptions import ValidationError
+
 from web.forms.BootStrapForm import BootStrapForm
 from web import models
 
@@ -8,3 +10,24 @@ class FolderModelForm(BootStrapForm, forms.ModelForm):
         model = models.FileRepository
         fields = ['name']
 
+    def __init__(self, request, parent_object, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.request = request
+        self.parent_object = parent_object
+
+    # 判断是否已有相同的文件名
+    def clean_name(self):
+        name = self.cleaned_data['name']
+
+        # 数据库判断 当前目录下 此文件是否已存在
+        queryset = models.FileRepository.objects.filter(file_type=2, name=name, project=self.request.tracer.project)
+        if self.parent_object:
+            exists = queryset.filter(parent=self.parent_object).exists()
+        else:
+            exists = queryset.filter(parent__isnull=True).exists()
+
+        # 如果存在表示 已有相同的文件名
+        if exists:
+            raise ValidationError('文件夹已存在')
+
+        return name
--- web/views/file.py
+++ web/views/file.py
@@ -7,19 +7,19 @@ from web.forms.file import FolderModelForm
 
 def file(request, project_id):
     ''' 文件列表 & 添加文件夹 '''
-    if request.method == "GET":
-        form = FolderModelForm()
-        return render(request, 'file/file.html', {'form': form})
-
     # 获取父级文件夹
     parent_object = None
     folder_id = request.GET.get('folder', "")  # 如果有值获取folder 如果没有值为空
     # 判断是否是十进制的值
     if folder_id.isdecimal():
-        parent_object = models.FileRepository.objects.filter(id=int(folder_id), file_type=2, project=request.tracer.project).first()
+        parent_object = models.FileRepository.objects.filter(id=int(folder_id), file_type=2,
+                                                             project=request.tracer.project).first()
+    if request.method == "GET":
+        form = FolderModelForm(request, parent_object)
+        return render(request, 'file/file.html', {'form': form})
 
     # 添加文件夹
-    form = FolderModelForm(request.POST)
+    form = FolderModelForm(request, parent_object, data=request.POST)
     if form.is_valid():
         form.instance.project = request.tracer.project
         form.instance.file_type = 2
