513d984558113960c352a0acaff570b869ef70d4
养乐多要吸管 <a102467@outlook.com>
2023-04-09 12:39:43 +0800

    Add problem information

--- web/views/issues.py
+++ web/views/issues.py
@@ -1,10 +1,25 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 from web.forms.issues import IssuesModelForm
 
 
 def issues(request, project_id):
+    if request.method == "GET":
+        form = IssuesModelForm(request)
 
-    form = IssuesModelForm(request)
+        return render(request, 'issues/issues.html', {'form': form})
 
-    return render(request, 'issues/issues.html', {'form': form})
+    # print(request.POST)
+    form = IssuesModelForm(request, data=request.POST)
+    if form.is_valid():
+        # 添加默认信息
+        form.instance.project = request.tracer.project
+        form.instance.creator = request.tracer.user
+
+        # 添加问题
+        form.save()
+        return JsonResponse({'status': True})
+
+    return JsonResponse({'status': False, 'error': form.errors})
+
