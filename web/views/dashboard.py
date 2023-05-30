df25bca163b06d4a716253324583c8e74138adf6
养乐多要吸管 <a102467@outlook.com>
2023-05-30 20:08:52 +0800

    Data statistics display

--- web/views/dashboard.py
+++ web/views/dashboard.py
@@ -1,6 +1,28 @@
+import collections
 
+from django.db.models import Count
 from django.shortcuts import render
 
-def dashboard(request, project_id):
+from web import models
 
-    return render(request, 'dashboard/dashboard.html')
+
+def dashboard(request, project_id):
+    ''' 概览 '''
+
+    # 问题数据处理
+    status_dict = collections.OrderedDict()
+    for key, text in models.Issues.status_choices:
+        status_dict[key] = {'text': text, 'count': 0}
+
+    issues_data = models.Issues.objects.filter(project_id=project_id).values('status').annotate(ct=Count('id'))
+
+    for item in issues_data:
+        status_dict[item['status']]['count'] = item['ct']
+
+    context = {
+        'status_dict': status_dict
+    }
+
+    return render(request, 'dashboard/dashboard.html', context)
+
+
