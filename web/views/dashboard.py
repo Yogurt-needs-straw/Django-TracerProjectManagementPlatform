fc3dd2bf08adad3a060bd275e401964877f6ea05
养乐多要吸管 <a102467@outlook.com>
2023-06-02 19:35:06 +0800

    Data statistics display ：project creator

--- web/views/dashboard.py
+++ web/views/dashboard.py
@@ -19,8 +19,14 @@ def dashboard(request, project_id):
     for item in issues_data:
         status_dict[item['status']]['count'] = item['ct']
 
+
+    # 项目成员
+    user_list = models.ProjectUser.objects.filter(project_id=project_id).values('user_id', 'user__username')
+
+
     context = {
-        'status_dict': status_dict
+        'status_dict': status_dict,
+        'user_list': user_list,
     }
 
     return render(request, 'dashboard/dashboard.html', context)
